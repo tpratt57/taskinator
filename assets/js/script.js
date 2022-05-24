d99b3f777662f68b8e76017b99da254cccb04b19
unknown <pratttrevor79@gmail.com>
2022-05-23 20:02:07 -0500

    adding function to properly delete a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content")
 
 var taskFormHandler = function (event) {
   event.preventDefault();
@@ -89,4 +90,22 @@ var createTaskActions = function (taskId) {
   return actionContainerEl;
 }
 
+var deleteTask = function (taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+var taskButtonHandler = function (event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")) {
+    // get the element's task id
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+}
+
+
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler)
